0b8d482f81aa41d34c679f1920c7ac39529a9f9b
Abdul-Hadi-Jalil <abdulhadijalil34@gmail.com>
2025-03-10 04:13:51 +0500

    commit

--- learning_logs/urls.py
+++ learning_logs/urls.py
@@ -1,4 +1,4 @@
-"""Defines URL patterns for learning_logs."""
+""" Define the url patterns for learning_logs"""
 
 from django.urls import path
 from . import views
@@ -8,8 +8,10 @@ app_name = 'learning_logs'
 urlpatterns = [
     # Home page
     path('', views.index, name='index'),
+    
     # Topics page to show all topics
     path('topics/', views.topics, name='topics'),
+    
     # Show the details of a certain topic
-    path(r'^topic/(?P<topic_id>\d+)/$', views.topic, name='topic')
+    path('topics/<int:topic_id>/', views.topic, name='topic'),
 ]
