aa263b2f8420ef36301baa09086f5afd9ffe5754
Abdul-Hadi-Jalil <abdulhadijalil34@gmail.com>
2025-03-09 16:35:54 +0500

    create a template for home page, add views some additions in urls.py and created another file urls.py inside learning_logs/templates, build a tempalte using index.html file

--- learning_logs/models.py
+++ learning_logs/models.py
@@ -18,4 +18,4 @@ class Entry(models.Model):
         verbose_name_plural = 'Entries'
 
     def __str__(self):
-        return self.text[:50] + '...'
+        return self.text[:50] + '...' if len(self.text) > 50 else self.text
