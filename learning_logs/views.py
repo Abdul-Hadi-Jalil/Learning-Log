7f4b2209820c9fff1b1bede3367efbd4f5350319
Abdul-Hadi-Jalil <abdulhadijalil34@gmail.com>
2025-03-10 11:22:54 +0500

    completed a basic web app

--- learning_logs/views.py
+++ learning_logs/views.py
@@ -1,14 +1,14 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Topic
 
 # Create your views here.
 
 def index(request):
-    """ The Home page for Learning Log"""
+    """ The Home page for Learning Log """
     return render(request, 'learning_logs/index.html')
 
 def topics(request):
-    """ The page that show all the topics """
+    """ The page that shows all topics """
     topics = Topic.objects.order_by('date_added')
     context = {'topics': topics}
     return render(request, 'learning_logs/topics.html', context)
@@ -17,5 +17,5 @@ def topic(request, topic_id):
     """ The page that shows the details of a topic """
     topic = Topic.objects.get(id=topic_id)
     entries = topic.entry_set.order_by('-date_added')
-    context = {"Topic": topic, "Entries": entries}
+    context = {"topic": topic, "entries": entries}  # ✅ Corrected dictionary keys
     return render(request, 'learning_logs/topic.html', context)
