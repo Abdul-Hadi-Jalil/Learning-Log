47fb86d8ba16921fdab581666ff3e0c42d0dfdaa
Abdul-Hadi-Jalil <abdulhadijalil34@gmail.com>
2025-03-11 03:34:00 +0500

    another page for new entries

--- learning_logs/views.py
+++ learning_logs/views.py
@@ -1,5 +1,8 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render
 from .models import Topic
+from .forms import TopicForm, EntryForm
+from django.http import HttpResponseRedirect
+from django.urls import reverse
 
 # Create your views here.
 
@@ -19,3 +22,37 @@ def topic(request, topic_id):
     entries = topic.entry_set.order_by('-date_added')
     context = {"topic": topic, "entries": entries}  # ✅ Corrected dictionary keys
     return render(request, 'learning_logs/topic.html', context)
+
+def new_topic(request):
+    """ Add new topic """
+    if request.method != 'POST':
+        # no data submitted, create a blank form
+        form = TopicForm()
+    else:
+        # post data submitted: process data
+        form = TopicForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return HttpResponseRedirect(reverse('learning_logs:topics'))
+    
+    context = {'form':form}
+    return render(request, 'learning_logs/new_topic.html', context)
+
+def new_entry(request, topic_id):
+    """Add a new entry for a particular topic."""
+    topic = Topic.objects.get(id=topic_id)
+
+    if request.method != 'POST':
+        # create a blank entry
+        form = EntryForm()
+    else:
+        form = EntryForm(data=request.POST)
+        if form.is_valid():
+            new_entry = form.save(commit=False)
+            new_entry.topic = topic
+            new_entry.save()
+            return HttpResponseRedirect(reverse('learning_logs:topic',
+                                        args=[topic_id]))
+    
+    context = {'topic': topic, 'form': form}
+    return render(request, 'learning_logs/new_entry.html', context)
