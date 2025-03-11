0790f193338fcb79a7cd4bba4de33527fc2bc7ec
Abdul-Hadi-Jalil <abdulhadijalil34@gmail.com>
2025-03-11 06:44:57 +0500

    completed the edit entry page

--- learning_logs/urls.py
+++ learning_logs/urls.py
@@ -19,5 +19,8 @@ urlpatterns = [
     path('new_topic/', views.new_topic, name='new_topic'),
 
     # Entries for new topics
-    path('new_entry/<int:topic_id>/', views.new_entry, name='new_entry' )
+    path('new_entry/<int:topic_id>/', views.new_entry, name='new_entry' ),
+
+    # editing existing entries
+    path('edit_entry/<int:entry_id>/', views.edit_entry, name='edit_entry'),
 ]
